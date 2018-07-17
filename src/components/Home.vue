16ef6e09c7c1824adb858ec9b843647756cbadee
nhult <n.hulttinen97@gmail.com>
2018-07-17 18:26:32 +0300

    progress on /home header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -30,7 +30,11 @@
           </div>
         </div>
       </div>
-      <button>Tweet</button>
+      <div class="Chirp__modal-toggle">
+        <button type="button" @click="chirpModal__toggle()">Chirp</button>
+        <div class="Chirp__modal" v-show="chirpModal" @click="chirpModal__toggle()">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -43,7 +47,8 @@ export default {
   data() {
     return {
       inputValue: '',
-      profileModal: false
+      profileModal: false,
+      chirpModal: true
     }
   },
   methods: {
@@ -56,6 +61,7 @@ export default {
       })
     },
     profileModal__toggle: function() { this.profileModal = !this.profileModal },
+    chirpModal__toggle: function() { this.chirpModal = !this.chirpModal },
     searchValue: function() {
       alert(this.inputValue)
     },
@@ -76,7 +82,7 @@ export default {
 .Header {
   display: flex;
   flex-flow: row nowrap;
-  justify-content: flex-start;
+  justify-content: flex-end;
   align-items: center;
 
   width: 100vw;
@@ -92,26 +98,25 @@ export default {
 
   & input[type="text"] {
     width: 12rem;
-    height: 1.5rem;
+    height: 1.8rem;
     border-radius: 1rem;
     padding-left: 10px;
+    font-size: 0.78rem;
+    margin: 0 20px 0 0;
 
     background: url(../assets/icons8-search-50-ccc.png) no-repeat scroll 7px 0;
     background-size: 20px;
     background-position: 95% 50%;
-  }
-
-  & .Header__nav-home {
-    margin-left: 50px;
+    background-color: rgba(127, 143, 166,.07);
+    border: solid 1px rgba(127, 143, 166,.2);;
   }
   & img {
-    margin-left: auto;
+    margin: 0 auto;
     width: 2rem;
-    margin: 0 50px;
     cursor: pointer;
   }
-  & button {
-    margin: 0 25px 0 auto;
+  & .Chirp__modal-toggle button {
+    margin: 0 22rem 0 0;
     width: 4.5rem;
     height: 2.1rem;
     border: 1px solid rgba(9, 132, 227, .7);
@@ -121,10 +126,21 @@ export default {
     color: white;
     font-weight: bold;
     font-size: 0.95em;
+
     &:hover {
       background: rgba(9, 132, 227, .8);
     }
   }
+  & .Chirp__modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vw;
+    background: rgba(0, 0, 0, .5);
+    cursor: pointer;
+  }
+
   & input {
     margin-left: auto;
   }
@@ -145,6 +161,9 @@ export default {
       color: rgba(9, 132, 227, 1);
     }
   }
+  & .Header__nav-home {
+    margin: 0 20px 0 22rem;
+  }
 
 
   & .Header__nav-profile {
@@ -154,7 +173,7 @@ export default {
     flex-flow: row nowrap;
     justify-content: center;
     align-items: center;
-    margin: 0 20px;
+    margin: 0 20px 0 0;
 
     & img {
       width: 100%;
@@ -170,7 +189,7 @@ export default {
       width: 12rem;
       top: 3rem;
       right: 0;
-      background: rgba(127, 143, 166,.15);
+      background: rgba(127, 143, 166,.07);
       z-index: 10;
       border-radius: 3px;
       -webkit-box-shadow: 0px 1px 1px 1px rgba(0,0,0,0.15);
